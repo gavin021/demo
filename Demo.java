b8ae505e5c75870d16d56a7a546a55fee4544f77
Gavin <liuchao021@gmail.com>
2025-01-04 14:45:19 +0900

    roll back Demo.java

--- Demo.java
+++ Demo.java
@@ -7,7 +7,6 @@ public class Demo{
         List<String> list = Arrays.asList("a", "b", "c");
         list.stream().forEach(item->System.out.println(item));
         list.stream().forEach(item->System.out.println(item));
-        list.stream().forEach(item->System.out.println(item));
         List<String> rsltList = 
             list.stream().filter(item->"b".equals(item)).map(item->item + "1").collect(Collectors.toList());
         System.out.println(rsltList);
