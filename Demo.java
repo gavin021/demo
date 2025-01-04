d16bf295e38c363747a06c29bc6d5e95827758ed
Gavin <liuchao021@gmail.com>
2025-01-04 14:17:45 +0900

    Add exam info

--- Demo.java
+++ Demo.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Demo{
     public static void main(String[] args){
-        System.out.println("hello,world");
-        System.out.println("hello,python");
+        List<String> list = Arrays.asList("a", "b", "c");
+        list.stream().forEach(item->System.out.println(item));
+        List<String> rsltList = 
+            list.stream().filter(item->"b".equals(item)).map(item->item + "1").collect(Collectors.toList());
+        System.out.println(rsltList);
     }
 }
